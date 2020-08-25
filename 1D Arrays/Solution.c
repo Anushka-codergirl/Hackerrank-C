110960220fb5779d05aae0357b25bc33c97b79e7
Anushka Raj <anush6633@gmail.com>
2020-08-25 20:36:51 +0530

    Create Solution.c

---
+++ 1D Arrays/Solution.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */  
+    int i,n,sum=0;
+ 
+    scanf("%d",&n);
+    int *arr = (int*)malloc(n * sizeof(int));
+    for(i = 0; i<n; i++)
+    {
+        scanf("%d",&arr[i]);
+         sum += arr[i];
+    }
+    
+    printf("%d",sum);
+    return 0;
+}
